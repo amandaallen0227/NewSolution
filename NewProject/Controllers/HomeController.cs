fcb7ecab41295a9cb65adf3d377aaff3e8537e97
Amanda Allen <amandaallen0227@gmail.com>
2021-12-08 16:32:45 -0600

    Serilog

--- NewProject/Controllers/HomeController.cs
+++ NewProject/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Serilog; // access to LoggerConfiguration()
+using Serilog.Events; //access to LogEventLevel
 
 namespace NewProject.Controllers
 {
@@ -16,6 +18,10 @@ namespace NewProject.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            var stackifyLogger = new LoggerConfiguration()
+                .WriteTo.Stackify(restrictedToMinimumLevel: LogEventLevel.Information)
+                .CreateLogger();
+            stackifyLogger.Information("-Serilog-Info-About");
 
             return View();
         }
@@ -23,7 +29,10 @@ namespace NewProject.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
+            var stackifyLogger = new LoggerConfiguration()
+                .WriteTo.Stackify(restrictedToMinimumLevel: LogEventLevel.Fatal)
+                .CreateLogger();
+            stackifyLogger.Information("-Serilog-Fatal-Contact");
             return View();
         }
     }
